7d53189e27e1c80a0075c2992d6ee5436e43b070
iobitt <sashabratm@gmail.com>
2022-01-25 11:23:45 +0300

    Доработки по статьям и комментариям

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_185122) do
+ActiveRecord::Schema.define(version: 2022_01_24_194842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_01_24_185122) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "public", null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_01_24_185122) do
     t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "public", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
