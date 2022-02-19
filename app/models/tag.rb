6df17f2da5ca0872b54067f4b12e53336cefa1eb
iobitt <sashabratm@gmail.com>
2022-02-19 22:24:59 +0300

    Добавляем тегу отношения и валидацию

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ApplicationRecord
+  has_one :user
+  has_and_belongs_to_many :tasks
+
+  validates user_id, presence: true
+  validates name, presence: true, uniqueness: { scope: :user }
 end
