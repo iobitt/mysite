19c65ea40d2fc67f37485a303200c6cd07b7df80
iobitt <sashabratm@gmail.com>
2022-02-19 22:27:56 +0300

    Правим опечатку

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,6 @@ class Tag < ApplicationRecord
   has_one :user
   has_and_belongs_to_many :tasks
 
-  validates user_id, presence: true
-  validates name, presence: true, uniqueness: { scope: :user }
+  validates :user_id, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user }
 end
