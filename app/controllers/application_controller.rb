88308082606d9a636d168d93b325b4bc7fe193d5
iobitt <sashabratm@gmail.com>
2022-02-23 12:14:24 +0300

    Редиректим на страницу логина, если пользователь не авторизован

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
 
-  before_action :find_user
+  before_action :find_user_or_redirect
 
   private
 
-  def find_user
+  def find_user_or_redirect
     @current_user = User.find_by_id(session[:user_id])
-    session[:user_id] = nil unless @current_user
+    unless @current_user
+      session[:user_id] = nil
+      redirect_to login_path if params[:controller] != 'auth' && params[:action] != 'login'
+    end
   end
 end
